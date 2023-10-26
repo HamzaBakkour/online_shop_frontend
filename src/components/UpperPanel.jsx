861c4c38db73060127980b87b2666189a8d2ad12
HamzaBakkour <alexander.x4.hb@outlook.com>
2023-10-26 17:45:51 +0200

    m-c

--- src/components/UpperPanel.jsx
+++ src/components/UpperPanel.jsx
@@ -12,41 +12,6 @@ import image_background from '../assets/images/poppy-hill-our.webp'
 
 function UpperPanel(){
 
-    const [ brightnessLevel, setBrightnessLevel ] = useState(1.7);
-
-    const listenScrollEvent = (event) => {
-        event.preventDefault();
-        const position = window.scrollY / parseFloat(window.innerHeight)
-
-
-        if (position > 0.1 && position < 0.2) {
-        return setBrightnessLevel(1.6)
-        } else if (position > 0.2 && position < 0.3) {
-        return setBrightnessLevel(1.3)
-        } else if (position > 0.3 && position < 0.4) {
-        return setBrightnessLevel(1.2)
-        } else if (position > 0.4 && position < 0.5) {
-        return setBrightnessLevel(1.1)
-        } else if (position > 0.5 && position < 0.6) {
-        return setBrightnessLevel(1.0)
-        } else if (position > 0.6 && position < 0.7) {
-        return setBrightnessLevel(0.9)
-        } else if (position > 0.7 && position < 0.8) {
-        return setBrightnessLevel(0.8)
-        } else if (position > 0.8 && position < 0.9) {
-        return setBrightnessLevel(0.7)
-        } else if (position > 0.9 && position < 1) {
-        return setBrightnessLevel(0.6)
-        } else if (position > 1) {
-        return setBrightnessLevel(0.5)}
-    }
-
-
-    useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    return () =>
-    window.removeEventListener('scroll', listenScrollEvent);
-    }, []);
 
     return (
         <>
@@ -63,10 +28,10 @@ function UpperPanel(){
                     position: 'relative',
                     top: '0',
                     left: '0',
-                    filter: `brightness(${brightnessLevel})`}}
+                    filter: `brightness(1.2)`}}
                     >
                     <Col md={12} className='d-flex flex-column justify-content-end align-items-center'>
-                        <Search className={brightnessLevel} />
+                        <Search />
                         <Categories />
                     </Col>
                 </Row>
